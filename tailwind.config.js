184085729d21cac1ba18b7194c0fdd38c88f34c0
s_f <1581180638@qq.com>
2025-01-08 17:36:37 +0800

    wip: :construction: 主题切换

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        base1: 'var(--bg-base1)',
+        base2: 'var(--bg-base2)',
+        active1: 'var(--bg-active1)',
+        active2: 'var(--bg-active2)'
+      },
+      textColor: {
+        color1: 'var(--text-color1)',
+        color2: 'var(--text-color2)',
+        colorHover: 'var(--text-color-hover)',
+        colorActive: 'var(--text-color-active)'
+      }
+    }
   },
-  plugins: [],
-}
-
+  plugins: []
+};
