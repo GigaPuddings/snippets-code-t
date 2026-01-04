5b972ce12670b686834226278f05d4b21059a728
GigaPuddings <87363568+GigaPuddings@users.noreply.github.com>
2026-01-04 13:47:12 +0800

    release: v1.3.1

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -20,8 +20,6 @@ declare module 'vue' {
     CustomSwitch: typeof import('./../components/UI/CustomSwitch.vue')['default']
     EditDialog: typeof import('./../pages/config/components/local/components/EditDialog.vue')['default']
     ElButton: typeof import('element-plus/es')['ElButton']
-    ElCollapse: typeof import('element-plus/es')['ElCollapse']
-    ElCollapseItem: typeof import('element-plus/es')['ElCollapseItem']
     ElConfigProvider: typeof import('element-plus/es')['ElConfigProvider']
     ElDatePicker: typeof import('element-plus/es')['ElDatePicker']
     ElDialog: typeof import('element-plus/es')['ElDialog']
